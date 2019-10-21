6c4056fff7a7d6ab944c88d41bbb3573783719ea
Yuan-EPAM <yuan_liao@epam.com>
2019-10-21 09:29:34 +0800

    added a module for importing images sources

---
+++ week-04/mini-project/gallery/src/Data.js
@@ -0,0 +1,9 @@
+const DATA = [
+    { id: 1, path: "./imgs/1.1.jpg" },
+    { id: 2, path: "./imgs/2.1.png" },
+    { id: 3, path: "./imgs/3.1.jpeg" },
+    { id: 4, path: "./imgs/4.1.png" },
+    { id: 5, path: "./imgs/5.1.jpeg" }
+];
+
+export default DATA;
