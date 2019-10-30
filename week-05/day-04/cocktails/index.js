60bc582344f5a1dc7d6de17190683177a8bc2fd5
Yuan-EPAM <yuan_liao@epam.com>
2019-10-30 12:16:52 +0800

    implemented filter cocktail type function

--- week-05/day-04/cocktails/index.js
+++ week-05/day-04/cocktails/index.js
@@ -7,14 +7,29 @@ const data = require('./data');
 app.set('view engine', 'ejs');
 app.use('/static', express.static('static'));
 
+function cocktailsFilter(type, data) {
+    let selectedCocktails;
+
+    if (type != undefined) {
+        selectedCocktails = data.filter(item => {
+            return item['contains'].includes(type);
+        })
+    }
+
+    return selectedCocktails;
+}
+
 app.get('/', (req, res) => {
+    let alcoholType = req.query.alcohol;
+    let selectedCocktails = cocktailsFilter(alcoholType, data.cocktails);
+
     res.render('home', {
         alcoholList: data.alcoholList,
-        cocktails : data.cocktails
+        cocktails: alcoholType ? selectedCocktails : data.cocktails,
+        alcoholType : alcoholType
     });
 });
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
-    console.log(data.alcoholList);
 });
