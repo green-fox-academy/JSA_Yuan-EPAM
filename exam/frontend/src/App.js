eefc9f8e571aaf8e567613dc3f52f15040050835
Yuan-EPAM <yuan_liao@epam.com>
2019-11-08 11:31:19 +0800

    fixed a npm err ELIFECYCLE before starte react \n (Methods : 1. npm cache clean --force 2. rm -rf ./node_modules 3. npm install )

---
+++ exam/frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import './App.css';
+
+import Doodle from './Doodle'
+
+function App() {
+  return (
+    <div className="App">
+      <Doodle />
+    </div>
+  );
+}
+
+export default App;
